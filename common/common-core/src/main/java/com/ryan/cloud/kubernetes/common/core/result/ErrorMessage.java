f35e48b7c26848feaac3540dc1b2b62ec4197f7c
ryan <hs_18312833057@yeah.net>
2021-02-26 14:36:56 +0800

    初始化项目

---
+++ common/common-core/src/main/java/com/ryan/cloud/kubernetes/common/core/result/ErrorMessage.java
@@ -0,0 +1,25 @@
+package com.ryan.cloud.kubernetes.common.core.result;
+
+/**
+ * 通用错误信息
+ *
+ * @author Ryan
+ * @version 1.0.0
+ * @date 2021-02-26 14:31
+ */
+public interface ErrorMessage {
+
+    /**
+     * 获取响应码
+     *
+     * @return 响应码
+     */
+    Integer getCode();
+
+    /**
+     * 获取响应信息
+     *
+     * @return 响应信息
+     */
+    String message();
+}
