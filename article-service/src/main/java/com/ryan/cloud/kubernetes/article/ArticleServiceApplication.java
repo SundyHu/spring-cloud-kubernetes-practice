7f0f55693ed85bbb6caa22aaec630468a730bf5d
ryan <hs_18312833057@yeah.net>
2021-03-05 14:55:31 +0800

    添加ArticleService

---
+++ article-service/src/main/java/com/ryan/cloud/kubernetes/article/ArticleServiceApplication.java
@@ -0,0 +1,20 @@
+package com.ryan.cloud.kubernetes.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ArticleServiceApplication
+ *
+ * @author hkc
+ * @version 1.0.0
+ * @date 2021-03-02 16:32
+ */
+@SpringBootApplication
+public class ArticleServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ArticleServiceApplication.class, args);
+    }
+}
---
+++ article-service/src/main/java/com/ryan/cloud/kubernetes/article/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.ryan.cloud.kubernetes.article.controller;
+
+import com.ryan.cloud.kubernetes.common.core.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * IndexController
+ *
+ * @author hkc
+ * @version 1.0.0
+ * @date 2021-03-02 16:32
+ */
+@RestController
+@Slf4j
+public class IndexController {
+
+    @GetMapping(value = {"", "/", "/index"})
+    public Result<Void> index(HttpServletRequest request) {
+
+        return Result.ok();
+    }
+}
